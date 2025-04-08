8bdd7f3c367127a9de78577694f9ebd6e41e1d1e
avaucher <avaucher@users.noreply.github.com>
2025-04-08 21:59:17 +0000

    deploy: dadaaf932ca62b5a3a57afe46bde60e719df9a80

---
+++ _next/static/chunks/app/_not-found/page-e97718b0feed4944.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[409,931,327,349,725],{2259:function(){}},function(n){n.O(0,[99,203,744],function(){return n(n.s=2259)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/science/page-7fdecfc201923f90.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[725,409,931,327,349],{2259:function(){}},function(n){n.O(0,[99,203,744],function(){return n(n.s=2259)}),_N_E=n.O()}]);
