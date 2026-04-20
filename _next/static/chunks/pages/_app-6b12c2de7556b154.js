309c6da97dc68322683ce4bcba470710e5af90bf
avaucher <avaucher@users.noreply.github.com>
2026-04-20 21:29:31 +0000

    deploy: deb83b616b854bd217e114b985e8e543f3dc21c4

---
+++ _next/static/chunks/pages/_app-6b12c2de7556b154.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4293:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6611)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4293),n(9883))),_N_E=_.O()}]);
